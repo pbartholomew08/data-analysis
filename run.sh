630b26d4cb6ba6b05271c8808ab25bc686722dcb
Paul Bartholomew <paul.bartholomew08@imperial.ac.uk>
2018-10-31 10:17:21 +0000

    Adding a 'run' script to build data analyses

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#        FILE: run.sh
+#      AUTHOR: Paul Bartholomew <paul.bartholomew08@imperial.ac.uk>
+# DESCRIPTION: Shell script to run post-processing notebooks, for example
+#                ./run.sh ./eCSE1002/non-boussinesq.org
+#
+
+emacs -Q --batch 
+
