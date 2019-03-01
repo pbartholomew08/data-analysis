c758535f981905cc10cc7d2079c4ac600c0d8d02
Paul Bartholomew <paul.bartholomew08@imperial.ac.uk>
2019-03-01 10:05:36 +0000

    Adding a Makefile for users to build tgv comparison
    Its not exactly perfect but it should do.

--- eCSE1303/tgv/plot_tgv.py
+++ eCSE1303/tgv/plot_tgv.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 
 def read_stats(filename):
@@ -6,22 +7,35 @@ def read_stats(filename):
   ke = []
 
   with open(filename, "r") as data:
-    next(data)
     for row in data:
-      words = row.split()
-      t.append(float(words[0]))
-      enst.append(float(words[1]))
-      ke.append(float(words[2]))
+      if not (row[0]=="#"):
+        words = row.split()
+        t.append(float(words[0]))
+        enst.append(float(words[1]))
+        ke.append(float(words[3]))
 
   return t, enst, ke
 def plot_stats(x3d_t, x3d_dat, x3d_lab, e3d_t, e3d_dat, e3d_lab,
                xlab, ylab, outfile, figsize=(5.0, 3.5)):
 
   plt.figure(figsize=figsize)
-  plt.save(outfile, bbox_inches="tight")
 
-x3d_t, x3d_enst, x3d_ke = read_stats("./x3d/statistics-clean")
-e3d_t, e3d_enst, e3d_ke = read_stats("./e3d/time_evol-clean.dat")
+  plt.plot(x3d_t, x3d_dat, label=x3d_lab)
+  plt.plot(e3d_t, e3d_dat, label=e3d_lab)
+
+  plt.xlabel(xlab)
+  plt.ylabel(ylab)
+  plt.legend()
+
+  plt.savefig(outfile, bbox_inches="tight")
+  plt.close()
+
+x3d_t, x3d_enst, x3d_ke = read_stats("./x3d/time_evol.dat")
+e3d_t, e3d_enst, e3d_ke = read_stats("./e3d/time_evol.dat")
+
+# Made an error in X3D, all time shifted by -dt=1e-2
+for i in range(len(x3d_t)):
+  x3d_t[i] += 1.0e-2
 
 plot_stats(x3d_t, x3d_enst, "X3D",
            e3d_t, e3d_enst, "Eric",
